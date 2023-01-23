164c885d8f0bf7bc0e3cf094aefeacf12b5fa9b9
ostandrey <ost.and.g@gmail.com>
2023-01-23 13:38:49 +0200

    full layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./index.html"],
+  // darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        'layout': 'auto 1fr auto'
+      }
+    },
+    // colors: {
+    //   'logo-main': '#9b0404'
+    // },
+    // fontFamily: {
+    //   main: ['Montserrat'], 
+    // }
   },
   plugins: [],
 }
